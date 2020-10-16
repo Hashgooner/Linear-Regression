d919fbb8fc40aaae12bc3b351f0e69a31a1228dd
HashGooner <harshshah1995kj@gmail.com>
2020-10-16 14:53:55 +0100

    Bar Chart for Categories added

--- Sentiment Analysis.R
+++ Sentiment Analysis.R
@@ -9,6 +9,7 @@ library(scales)
 library(reshape2)
 library(dplyr)
 library(stringr)
+library(plotly)
 
 newsHeadlineData <- read.csv("C:/Users/harsh/Downloads/india-news-headlines.csv")
 head(newsHeadlineData)
@@ -27,14 +28,18 @@ par(mfrow=c(1,1))
 newsHeadlineData$headline_category[newsHeadlineData$headline_category=='unknown'] <- ''
 unique(newsHeadlineData$headline_category)
 newsHeadlineData$mainCategory <- sub('\\..*','',newsHeadlineData$headline_category)
-mainCategoryFreq <- table(newsHeadlineData$mainCategory)
-mainCategoryFreq
+unique(newsHeadlineData$mainCategory)
+# mainCategoryFreq <- table(newsHeadlineData$mainCategory)
+# mainCategoryFreq
 str_trim(newsHeadlineData$headline_category,side = 'both')
 
 newsHeadlineData_split <- split(newsHeadlineData, newsHeadlineData$year)
-newsHeadlineData_2001 <- newsHeadlineData_split$'2001'
+newsHeadlineData_2001 <- newsHeadlineData_split$'2002'
 newsHeadlineData_2001split <- split(newsHeadlineData_2001, newsHeadlineData_2001$month)
 newsHeadlineData_2001split_1 <- newsHeadlineData_2001split$'1'
+newsHeadlineData_2001split_1$mainCategory[newsHeadlineData_2001split_1$mainCategory==''] <- 'NA'
+mainCategoryFreq <- table(newsHeadlineData_2001split_1$mainCategory)
+barplot(mainCategoryFreq, las = 2)
 text <- newsHeadlineData_2001split_1$headline_text
 docs <- Corpus(VectorSource(text))
 docs <- tm_map(docs,tolower)
@@ -60,34 +65,3 @@ wordcloud2(w,shape = 'circle')
 s <- get_nrc_sentiment(newsHeadlineData_2001split_1$headline_text)
 head(s)
 barplot(colSums(s),las=2)
-
-# newsHeadlineData_split <- split(newsHeadlineData, newsHeadlineData$year)
-# newsHeadlineData_2001 <- newsHeadlineData_split$'2001'
-# newsHeadlineData_2001split <- split(newsHeadlineData_2001, newsHeadlineData_2001$month)
-# newsHeadlineData_2001split_1 <- newsHeadlineData_2001split$'2'
-# 
-# text <- newsHeadlineData_2001split_1$mainCategory
-# docs <- Corpus(VectorSource(text))
-# docs <- tm_map(docs,tolower)
-# docs <- tm_map(docs,removePunctuation)
-# docs <- tm_map(docs,removeNumbers)
-# docs <- tm_map(docs,removeWords,stopwords('english'))
-# docs <- tm_map(docs, stripWhitespace)
-# inspect(docs[1:5])
-# 
-# tdm <- TermDocumentMatrix(docs)
-# tdm <- as.matrix(tdm)
-# w <- rowSums(tdm)
-# w <- subset(w,w>=40)
-# barplot(w,las = 2)
-# w <- sort(rowSums(tdm), decreasing = TRUE)
-# set.seed(222)
-# wordcloud(words = names(w), freq = w, max.words = 100, min.freq = 5)
-# 
-# w <- data.frame(names(w),w)
-# colnames(w) <- c('word','freq')
-# wordcloud2(w,shape = 'circle')
-# 
-# s <- get_nrc_sentiment(newsHeadlineData_2001split_1$mainCategory)
-# head(s)
-# barplot(colSums(s),las=2)

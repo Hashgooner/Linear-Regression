447caab0298f039bc059e8b4372a495b398bc877
HashGooner <harshshah1995kj@gmail.com>
2020-10-16 17:16:26 +0100

    Sub categories added

--- Sentiment Analysis.R
+++ Sentiment Analysis.R
@@ -24,22 +24,37 @@ summary(newsHeadlineData)
 dplyr::mutate(newsHeadlineData$year <- lubridate::year(newsHeadlineData$publish_date))
 dplyr::mutate(newsHeadlineData$month <- lubridate::month(newsHeadlineData$publish_date))
 dplyr::mutate(newsHeadlineData$day <- lubridate::day(newsHeadlineData$publish_date))
-par(mfrow=c(1,1))
+newsHeadlineData$headline_category <- str_trim(newsHeadlineData$headline_category,side = 'both')
+newsHeadlineData$headlineLen <- apply(newsHeadlineData,2,nchar)[,3]
+h <- hist(newsHeadlineData$headlineLen, main = 'Histogram for Headline Length', col = '#add8e6', 
+          xlab = 'Headline Length', breaks = 15)
+by_year <- count(newsHeadlineData %>% group_by(year))
+barplot(by_year$n, names.arg=by_year$year, las=2, xlab='Years',ylab = 'Number of Articles', col='black',
+        main = 'Number of articles published in a year')
+by_month <- count(newsHeadlineData %>% group_by(month))
+barplot(by_month$n, names.arg =  by_month$month, las=2, xlab='Months',ylab = 'Number of Articles', col='black',
+        main = 'Number of articles published in specific month')
+
 newsHeadlineData$headline_category[newsHeadlineData$headline_category=='unknown'] <- ''
-unique(newsHeadlineData$headline_category)
 newsHeadlineData$mainCategory <- sub('\\..*','',newsHeadlineData$headline_category)
-unique(newsHeadlineData$mainCategory)
-# mainCategoryFreq <- table(newsHeadlineData$mainCategory)
-# mainCategoryFreq
-str_trim(newsHeadlineData$headline_category,side = 'both')
+newsHeadlineData$SubmainCategory <- sub('\\w+\\.*','',newsHeadlineData$headline_category)
+newsHeadlineData$SubmainCategory <- sub('\\..*','',newsHeadlineData$SubmainCategory)
+mainCategoryFreq <- sort(table(newsHeadlineData$mainCategory), decreasing = T)
+SubmainCategoryFreq <- sort(table(newsHeadlineData$SubmainCategory), decreasing = T)
+barplot(mainCategoryFreq[0:10], las=2, xlab = 'Main Category', ylab = 'Frequency', col = 'black',
+        main = 'Frequency chart of main categories (Top 10)')
+barplot(SubmainCategoryFreq[0:10], las=2, xlab = 'Sub Main Category', ylab = 'Frequency', col = 'black',
+        main = 'Frequency chart of sub main categories (Top 10)')
+
 
 newsHeadlineData_split <- split(newsHeadlineData, newsHeadlineData$year)
 newsHeadlineData_2001 <- newsHeadlineData_split$'2002'
 newsHeadlineData_2001split <- split(newsHeadlineData_2001, newsHeadlineData_2001$month)
 newsHeadlineData_2001split_1 <- newsHeadlineData_2001split$'1'
-newsHeadlineData_2001split_1$mainCategory[newsHeadlineData_2001split_1$mainCategory==''] <- 'NA'
+
 mainCategoryFreq <- table(newsHeadlineData_2001split_1$mainCategory)
 barplot(mainCategoryFreq, las = 2)
+
 text <- newsHeadlineData_2001split_1$headline_text
 docs <- Corpus(VectorSource(text))
 docs <- tm_map(docs,tolower)
